006c0b85b37e5adf1405dac8b97a3cdf1d50cd44
Wesley Corner <wescorner@gmail.com>
2022-08-25 21:47:11 +0000

    add sold out badge on sold out products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,10 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
+
+    if params[:quantity] == 0
+      
+    end
   end
 
   def show
