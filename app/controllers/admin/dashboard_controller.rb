8720d2c1a386cdabdf81f08e5849dd337dc556f1
Wesley Corner <wescorner@gmail.com>
2022-08-26 01:34:57 +0000

    fix admin basic auth

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class Admin::DashboardController < ApplicationController
-  # http_basic_authenticate_with name: ENV['NAME'], password: ENV['PASSWORD']
+  http_basic_authenticate_with name: ENV["NAME"], password: ENV["PASSWORD"]
   
   def show
   end
