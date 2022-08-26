c0c310fd30d4f34723e176005f4a467b1f4fe44e
Wesley Corner <wescorner@gmail.com>
2022-08-26 04:58:06 +0000

    create User model and migration

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,5 @@
 class Admin::CategoriesController < ApplicationController
-  # http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
+  http_basic_authenticate_with name: ENV["NAME"], password: ENV["PASSWORD"]
 
   def index
     @categories = Category.order(id: :asc).all
